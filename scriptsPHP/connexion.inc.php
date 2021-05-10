8ddf3df3a89e57e040fa34eaea8d0bcfeac0f286
Simon Éveillé <eveillesimon@gmail.com>
2021-05-10 18:14:52 +0200

    changement d'attribut pour voir les erreurs

--- scriptsPHP/connexion.inc.php
+++ scriptsPHP/connexion.inc.php
@@ -7,6 +7,7 @@ $user =  'theodore';
 $pass =  'brocoli';
 try {
     $cnx = new PDO('pgsql:host=seveille.dev;dbname=testTed', $user, $pass);
+    $cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "ERREUR : La connexion a échouée";
 
@@ -14,6 +15,6 @@ try {
  * page html. Attention c'est utile pour débugger mais cela affiche des
  * informations potentiellement confidentielles donc éviter de le faire pour un
  * site en production.*/
-    //    echo "Error: " . $e;
+    // echo "Error: " . $e;
 
 }
